eff12b6f7f877c4e05bf50d1a9bf8f1cc4b99d0f
KG <kimkiwonn@gmail.com>
2023-08-04 01:17:15 +0900

    Add solve problem2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    var answer = StringBuilder(cryptogram)
+
+    var removedIndex: Set<Int>
+    var removedCount = 0
+    do {
+        removedIndex = mutableSetOf()
+        removedCount = 0
+
+        for(i in 0 until answer.length - 1) {
+            if (answer[i] == answer[i + 1]) {
+                removedIndex.add(i)
+                removedIndex.add(i + 1)
+            }
+        }
+
+        for(i in removedIndex) {
+            answer.deleteAt(i - removedCount)
+            removedCount++
+        }
+    } while(removedCount != 0)
+
+    return answer.toString()
 }
