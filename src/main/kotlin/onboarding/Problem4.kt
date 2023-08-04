b0c9e6e2418718d9c6cd864d7da092170e437fda
KG <kimkiwonn@gmail.com>
2023-08-04 20:36:30 +0900

    Add solve problem4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,15 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val sb = java.lang.StringBuilder(word)
+    return sb.map { it.greenFrog() }.joinToString("")
+}
+
+// map 함수는 List<map반환타입> 으로 변경시킴
+fun Char.greenFrog(): Char {
+    return when {
+        isUpperCase() -> ('Z'.code - (this.code - 'A'.code)).toChar()
+        isLowerCase() -> ('z'.code - (this.code - 'a'.code)).toChar()
+        else -> this
+    }
 }
