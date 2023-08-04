ca2627453896b187509b8a7a5398a2ff0e89f276
KG <kimkiwonn@gmail.com>
2023-08-04 21:54:20 +0900

    Add solve problem5

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,14 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    var money = money
+    var answer: MutableList<Int> = mutableListOf()
+    val bills = listOf(50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1)
+
+    for (i in bills.indices) {
+        answer.add(money / bills[i])
+        money %= bills[i]
+    }
+
+    return answer
 }
