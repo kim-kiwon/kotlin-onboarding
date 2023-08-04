eef3d09af06a3f26030aeb0778b6d5c87dc5893a
KG <kimkiwonn@gmail.com>
2023-08-04 20:25:26 +0900

    Add solve problem3

--- src/main/kotlin/onboarding/Problem3.kt
+++ src/main/kotlin/onboarding/Problem3.kt
@@ -1,5 +1,27 @@
 package onboarding
 
 fun solution3(number: Int): Int {
-    TODO("프로그램 구현")
+    var answer = 0
+
+    for (i in 1..number) {
+        answer += getClapCount(i)
+    }
+
+    return answer
+}
+
+fun getClapCount(number: Int): Int {
+    val intList = number.toString().toList().map { it.digitToInt() }
+
+    var count = 0
+    intList.forEach{
+        when (it) {
+            3 -> count++
+            6 -> count++
+            9 -> count++
+            else -> Unit
+        }
+    }
+
+    return count
 }
